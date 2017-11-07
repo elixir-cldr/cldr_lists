a2be8382c231a0d79abad7875c2884f14efae060
Kip Cole <kipcole9@gmail.com>
2017-11-08 00:56:57 +1100

    Update to ex_cldr 0.12.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CldrLists.Mixfile do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
 
   def project do
     [
@@ -35,8 +35,8 @@ defmodule CldrLists.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_cldr, path: "../cldr"},
-      {:ex_cldr_numbers, path: "../cldr_numbers"},
+      {:ex_cldr, "~> 0.12.0"},
+      {:ex_cldr_numbers, "~> 0.3.2"},
       {:ex_doc, ">= 0.18.1", only: :dev}
     ]
   end
